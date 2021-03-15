f3792968667a7551a9cdedc1112aabf8b417ea9a
jamsheriff <jamsheriff@mail.ru>
2021-03-15 17:32:53 +0300

    Initial Commit

---
+++ BMI Calculator/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Sherif Musa on 30.01.2020.
+//  Copyright © 2020  Sherif Musa. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
+
---
+++ BMI Calculator/ResultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Sherif Musa on 30.01.2020.
+//  Copyright © 2020  Sherif Musa. All rights reserved.
+//
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
